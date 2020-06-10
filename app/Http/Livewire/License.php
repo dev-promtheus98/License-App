3684a2d86a3bfc1ee6759381a8b118bf5e31e6d1
dev-promtheus98 <ozh98@protonmail.com>
2020-06-10 02:29:34 +0000

    Refactoring data bindings and adding pluralize text on view

--- app/Http/Livewire/License.php
+++ app/Http/Livewire/License.php
@@ -3,30 +3,16 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Service\LicensePriceCalculator;
 
 class License extends Component
 {
     public $teamSize = 2;
 
-    public $amount = 30;
-
-    public function mount()
-    {
-        $this->amount = $this->calculateAmount($this->teamSize);
-    }
-
-    public function updatedTeamSize(int $value)
-    {
-        $this->amount = $this->calculateAmount($value);
-    }
-
     public function render()
     {
-        return view('livewire.license');
-    }
-
-    private function calculateAmount(int $teamSize)
-    {
-        return $teamSize <= 5 ? $teamSize * 15 : 50 + ($teamSize - 5) * 10;
+        return view('livewire.license', [
+            'amount' => $this->amount = LicensePriceCalculator::calculatePrice($this->teamSize)
+        ]);
     }
 }
---
+++ app/Service/LicensePriceCalculator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Service;
+
+class LicensePriceCalculator
+{
+    public static function calculatePrice(int $teamSize): float
+    {
+        return $teamSize <= 5
+            ? $teamSize * 15
+            : 50 + ($teamSize - 5) * 10;
+    }
+}
